59184853c202f87ae113a7711f1bd2708a3ca6ad
Rafael <rmedinap@gmail.com>
2019-04-17 21:03:08 -0500

    Prepare adapter for List of Person

---
+++ app/src/main/java/pe/gob/saludpol/listaapp/adapter/PersonAdapter.java
@@ -0,0 +1,30 @@
+package pe.gob.saludpol.listaapp.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+import pe.gob.saludpol.listaapp.model.Person;
+
+public class PersonAdapter extends RecyclerView.Adapter<PersonAdapter.PersonViewHolder> {
+
+    private List<Person> lista;
+    
+    @NonNull
+    @Override
+    public PersonAdapter.PersonViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PersonAdapter.PersonViewHolder personViewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
