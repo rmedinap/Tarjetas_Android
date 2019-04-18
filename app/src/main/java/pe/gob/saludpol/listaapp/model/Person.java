144f019fcbf2af41683808714a2add84c6adce0e
Rafael <rmedinap@gmail.com>
2019-04-17 20:45:49 -0500

    Create class Person

---
+++ app/src/main/java/pe/gob/saludpol/listaapp/model/Person.java
@@ -0,0 +1,57 @@
+package pe.gob.saludpol.listaapp.model;
+
+public class Person {
+    private String nombre;
+    private String dni;
+    private int edad;
+    private String biografia;
+    private int idfoto;
+
+    public Person(String nombre, String dni, int edad, String biografia, int idfoto) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.edad = edad;
+        this.biografia = biografia;
+        this.idfoto = idfoto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getBiografia() {
+        return biografia;
+    }
+
+    public void setBiografia(String biografia) {
+        this.biografia = biografia;
+    }
+
+    public int getIdfoto() {
+        return idfoto;
+    }
+
+    public void setIdfoto(int idfoto) {
+        this.idfoto = idfoto;
+    }
+}
